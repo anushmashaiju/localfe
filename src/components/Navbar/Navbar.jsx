244ddc92e3a13e4d2ef8a2a73aaa6ad38a96a97f
anushmashaiju <anushma2015@gmail.com>
2024-03-25 20:55:47 +0530

    responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-// Navbar.jsx
+// Navbar.js
+
 import React, { useContext, useEffect, useState } from 'react';
 import "./Navbar.css";
 import { Bookmark, Edit, HelpCenter, Notifications, Search, Settings } from '@mui/icons-material';
@@ -8,7 +9,7 @@ import { Link } from 'react-router-dom';
 import { Logout } from '../../context/AuthActions';
 import LogoutIcon from '@mui/icons-material/Logout';
 import axios from 'axios';
-import { BASE_URL } from '../../constants/constants';
+
 
 function Navbar({ setSelectedLocation }) {
     const { user, dispatch } = useContext(AuthContext);
@@ -19,27 +20,37 @@ function Navbar({ setSelectedLocation }) {
     const [todaysEvents, setTodaysEvents] = useState([]);
     const [tomorrowsEvents, setTomorrowsEvents] = useState([]);
     const [showNotifications, setShowNotifications] = useState(false); // State to control visibility of notifications
-
+    const [showNavbarRight, setShowNavbarRight] = useState(false); // State to control visibility of right part of the navbar
+    
     const fetchTodaysAndTomorrowsEvents = async () => {
         try {
-          const res = await axios.get(`${BASE_URL}/api/events/todaysAndTomorrowsEvents`);
-          if (res.status === 200) {
-            const { todaysEvents, tomorrowsEvents } = res.data;
-            setTodaysEvents(todaysEvents);
-            setTomorrowsEvents(tomorrowsEvents);
-          } else {
-            console.error('Failed to fetch today\'s and tomorrow\'s events. Unexpected response:', res);
-          }
+            const res = await axios.get('http://localhost:8800/api/events/todaysAndTomorrowsEvents');
+            if (res.status === 200) {
+                const { todaysEvents, tomorrowsEvents } = res.data;
+                setTodaysEvents(todaysEvents);
+                setTomorrowsEvents(tomorrowsEvents);
+            } else {
+                console.error('Failed to fetch today\'s and tomorrow\'s events. Unexpected response:', res);
+            }
         } catch (error) {
-          console.error('Error fetching today\'s and tomorrow\'s events:', error);
+            console.error('Error fetching today\'s and tomorrow\'s events:', error);
         }
-      };
-      
-      useEffect(() => {
-        fetchTodaysAndTomorrowsEvents();
-      }, []);
+    };
 
-      const handleNotificationClick = () => {
+    useEffect(() => {
+        fetchTodaysAndTomorrowsEvents();
+    }, []);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+    const handleLogout = () => {
+
+        // Dispatch the logout action
+        dispatch(Logout());
+    };
+
+    const handleNotificationClick = () => {
         // Toggle visibility of notifications
         setShowNotifications(!showNotifications);
     };
@@ -51,13 +62,7 @@ function Navbar({ setSelectedLocation }) {
         );
     };
 
-    const toggleModal = () => {
-        setIsModalOpen(!isModalOpen);
-    };
-    const handleLogout = () => {
-        // Dispatch the logout action
-        dispatch(Logout());
-    };
+
     const handleLocationButtonClick = () => {
         setDistrictsDropdown(!districtsDropdown);
     };
@@ -69,6 +74,7 @@ function Navbar({ setSelectedLocation }) {
     };
 
     const allDistricts = ["Thiruvananthapuram", "Kollam", "Pathanamthitta", "Alappuzha", "Kottayam", "Idukki", "Ernakulam", "Thrissur", "Palakkad", "Malappuram", "Kozhikode", "Wayanad", "Kannur", "Kasargod"];
+
 
     return (
         <div className='navbarContainer'>
@@ -83,7 +89,8 @@ function Navbar({ setSelectedLocation }) {
                 </div>
             </div>
 
-            <div className="navbarRight">
+            <div className={`navbarRight ${showNavbarRight ? 'active' : ''}`}>
+            <div className="navbarLinksContainer">
                 <Link to="/" className='navbarLink'>Home</Link>
                 <Link to="/AddPost" className='navbarLink'>Profile</Link>
                 <span className='navbarLocation' onClick={handleLocationButtonClick}>
@@ -98,6 +105,13 @@ function Navbar({ setSelectedLocation }) {
                         ))}
                     </div>
                 )}
+</div>
+            </div>
+
+            <div className="toggleNavbarRight" onClick={() => setShowNavbarRight(!showNavbarRight)}>
+                <span className="toggleLine"></span>
+                <span className="toggleLine"></span>
+                <span className="toggleLine"></span>
             </div>
 
             <div className="navbarIcons">
@@ -120,32 +134,39 @@ function Navbar({ setSelectedLocation }) {
             </div>
 
             <div className="dropdown">
-                <img src={user.profilePicture ? PF + user.profilePicture : PF + "dp/default-avatar-profile-icon-social-600nw-1677509740.webp"} alt="" className='navbarImg' />
+                <img
+                    src={user?.profilePicture ? PF + user.profilePicture : PF + "dp/default-avatar-profile-icon-social-600nw-1677509740.webp"}
+                    alt=""
+                    className='navbarImg'
+                />
 
                 <div className="dropdown-content">
                     <a href="#1" onClick={toggleModal}>
                         <li className="dropdownListItem">
                             <Edit className='dropdownIcon' />
                             <span className='dropdownListItemText'>Edit</span>
-                        </li></a>
-                    <a href="#2"><li className="dropdownListItem">
+                        </li>
+                    </a>
+
+                    <a href="#3"><li className="dropdownListItem">
                         <Bookmark className='dropdownIcon' />
                         <span className='dropdownListItemText'>Bookmark</span>
                     </li></a>
-                    <a href="#3"><li className="dropdownListItem">
+                    <a href="#4"><li className="dropdownListItem">
                         <HelpCenter className='dropdownIcon' />
                         <span className='dropdownListItemText'>Help Center</span>
                     </li></a>
-                    <a href="#4"><li className="dropdownListItem">
+                    <a href="#5"><li className="dropdownListItem">
                         <Settings className='dropdownIcon' />
                         <span className='dropdownListItemText'>Settings</span>
                     </li></a>
-                    <a href="#5" onClick={handleLogout}><li className="dropdownListItem">
+                    <a href="#6" onClick={handleLogout}><li className="dropdownListItem">
                         <LogoutIcon className='dropdownIcon' />
                         <span className='dropdownListItemText'>LogOut</span>
                     </li></a>
                 </div>
             </div>
+
             <EditProfileModal isOpen={isModalOpen} onRequestClose={toggleModal} />
             <span className='user' title={user.userName}>{user.userName}</span>
         </div>
